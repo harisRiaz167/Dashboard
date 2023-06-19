f9f30cc66e76df16de20d1fe91c7cf53233ce920
harisRiaz167 <harisriaz167@gmail.com>
2023-06-19 11:26:49 +0500

    updated price compaision

--- my_test.py
+++ my_test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from getdata import getLineChart, getPieChart,getbarchart,getbarchartcust,getTotalsales
+import plotly.graph_objects as go
+from getdata import getLineChart, getPieChart,getbarchart,getpiechartcust,getTotalsales
 
 st.set_page_config(page_title = 'Sales Dashboard', layout='wide')
 df = pd.read_csv('./data/final.csv')
@@ -32,7 +33,27 @@ with fig3:
    st.plotly_chart(getbarchart(df,options))
 with fig4:
     st.subheader('Top Customers id')
-    st.plotly_chart(getbarchartcust(df,options))
+    st.plotly_chart(getpiechartcust(df,options))
 
+# Price Comparison of brands
+data = df.drop('date',axis=1)
+start_date = '2019-01-01'
+data['final_date'] = pd.date_range(start=start_date, periods=len(data), freq='D')
+st.header('Price comparison of brands')
+def plot():
 
+    df = data
 
+    clist = df["brand"].value_counts().head(10).reset_index()
+
+    brands = st.multiselect("Select brand", clist)
+    st.header("You selected: {}".format(", ".join(brands)))
+
+    dfs = {brand: df[df["brand"] == brand] for brand in brands}
+
+    fig = go.Figure()
+    for brand, df in dfs.items():
+          fig = fig.add_trace(go.Scatter(x=df["final_date"], y=df["price"], name=brand))
+    st.plotly_chart(fig, use_container_width=True)
+
+plot()
